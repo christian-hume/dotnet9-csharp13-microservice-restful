585c13f1632838e7178589245c7d3faf87da183c
christian-hume <christian-hume@hotmail.com>
2026-01-01 15:01:04 -0300

    feat: Create project for ASP.NET Core 9.0.0 and C# 13.0 Signed-off-by: Christian Hume <christian-hume@outlook.com>

---
+++ DotNet9.CSharp13.Microservice.Restful/Services/UserService.cs
@@ -0,0 +1,55 @@
+﻿using DotNet9.CSharp13.Microservice.Restful.Models;
+using DotNet9.CSharp13.Microservice.Restful.Repositories;
+
+namespace DotNet9.CSharp13.Microservice.Restful.Services
+{
+    /**
+     *  UserService.cs
+     * 
+     *  Author   Hume Cristian
+     *  Version  1.0.0
+     *  Created  2024-11-18
+     *  Modified 2024-11-18
+     */
+    public class UserService
+    {
+        #region [PRIVATE_FIELDS]
+        private readonly UserRepository _userRepository;
+        #endregion [PRIVATE_FIELDS]
+
+        #region [PUBLIC_CONSTRUCTORS]
+        public UserService()
+        {
+            _userRepository = new UserRepository();
+        }
+        #endregion [PUBLIC_CONSTRUCTORS]
+
+        #region [PUBLIC_METHODS]
+        public Int32 Create(UserModel userModel)
+        {
+            return _userRepository.Create(userModel);
+        }
+
+        public List<UserModel> FindAll()
+        {
+            return _userRepository.FindAll();
+        }
+
+        public UserModel? FindById(Int128 id)
+        {
+            return _userRepository.FindById(id);
+        }
+
+        public void Update(Int32 id, UserModel userModel)
+        {
+            _userRepository.Update(id, userModel);
+        }
+
+        public void Delete(Int32 id)
+        {
+            _userRepository.Delete(id);
+        }
+        #endregion [PUBLIC_METHODS]
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # dotnet9-csharp13-microservice-restful
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Installation](#installation)
+* [Authors](#authors)
+* [Version](#version)
+
+## General info
 Microservice RESTful project whit dotNET 9.0.305 and C# 13.0
+
+## Technologies
+Project is created with:
+* dotNET 9.0.305
+* C# 13.0
+
+## Installation
+To run this project, install it locally using git:
+
+1.- The first step is to clone the repository using the following command
+```
+$ git clone https://github.com/christian-hume/dotnet9-csharp13-microservice-restful.git
+```
+
+2.- Open project with Visual Studio Code
+```
+/dotnet9-csharp13-microservice-restful
+```
+
+3.- Run project with Visual Studio Code
+```
+Run file /Program.cs
+```
+
+3.- To test if the project is running, you should access the following URL in a web browser
+```
+http://localhost:8080/actuator/health
+```
+
+## Author
+Name and contact info
+* Christian Hume (DEVs)
+* username-developer@email.com
+
+## Version
+* 1.0.0
+
+
+
+
+
+https://localhost:7036/WeatherForecast
